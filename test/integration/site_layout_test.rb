00b7f424c984d67cbedd9ba8a1b233af5b557a44
Miroslav <miroslavr@protonmail.com>
2019-12-06 02:26:35 +0100

    Test gems

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -5,5 +5,6 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
+    assert_select "a[href=?]", signup_path
   end
 end
